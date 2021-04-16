90927b03a1d40f73058dd05125e0fbd9a73cdec2
Janne Mattila <janne.mattila@hotmail.fi>
2021-04-16 15:40:02 +0300

    Add placeholder project

---
+++ src/WebhookController/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebhookController
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
